4f6c7547793ca9df87c36aec8f31bfdfa8519ded
Thrishank123 <125119917+Thrishank123@users.noreply.github.com>
2024-11-25 22:53:51 +0530

    Added Signup

--- DBMS Project/index.js
+++ DBMS Project/index.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('public')); // For static files like CSS, images, etc.
 
 app.set('view engine', 'ejs');
 
+// Route for Login Page
 app.get('/', (req, res) => {
   res.render('Login');
 });
 
+// Route for Signup Page
+app.get('/signup', (req, res) => {
+  res.render('Signup');
+});
+
+// Start the server
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
