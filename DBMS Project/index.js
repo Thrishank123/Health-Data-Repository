5dc41aeffeac6e885e2f5e62f7646e0795c5ccbb
Vinaytarigoppula <vinaytarigoppula@gmail.com>
2024-11-25 22:56:33 +0530

    backend work

--- DBMS Project/index.js
+++ DBMS Project/index.js
@@ -1,14 +1,46 @@
-const express = require('express');
-const app = express();
+import express from "express";
+import bodyParser from "body-parser";
+import path from "path";
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+import { fileURLToPath } from "url";
+import bcrypt from "bcrypt";
+import session from "express-session"; // Import express-session
 
-app.use(express.static('public'));
-
-app.set('view engine', 'ejs');
-
-app.get('/', (req, res) => {
-  res.render('Login');
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+let db;
+db = await open({
+    filename: './shopkeep.db', // Corrected typo
+    driver: sqlite3.Database,
 });
 
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+  res.render('Login.ejs');
+});
+app.post("/login",async(req,res)=>{
+  const username=req.body("username");
+  const password=req.body("password");
+  const row = await db.get("SELECT * FROM Patient WHERE username = ?", [username]);
+  if(row)
+  {
+    const isMatch = await bcrypt.compare(password, row.password);
+        if (isMatch) {
+            res.redirect("/index.ejs"); 
+        } else {
+            res.send("Password not matched");
+        }
+    } else {
+        res.send("User not found");
+    }
+  }
+  
+)
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
